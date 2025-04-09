5bd1b50de4d3f26679bde267fcd8a77e7790aea1
Emilse Arias <emilsearias@gmail.com>
2025-04-09 11:00:08 -0300

    agregamos un ejemplo sencillo de condicional, para saber si una persona es mayor de edad

---
+++ js/2-EjercicioCondicionales.js
@@ -0,0 +1,8 @@
+/* Crear un programa que reciba la nota de un examen final (número entre 0 y 100) y muestre un mensaje según el desempeño del estudiante:
+Si la nota es menor a 0 o mayor a 100, mostrar: "Nota inválida".
+Si la nota está entre 0 y 59, mostrar: "Desaprobado".
+Si la nota está entre 60 y 79:
+Si la nota es mayor o igual a 70, mostrar: "Aprobado con buen desempeño".
+Si no, mostrar: "Aprobado con lo justo".
+Si la nota está entre 80 y 100, mostrar: "Excelente desempeño".*/
+
--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -19,3 +19,13 @@ if(condicion logica){
     todas las lineas de codigo que quiero ejecutar si no se cumple la condicion
 }
 */
+
+//Pedir la edad y decir si es mayor de edad: Crear un algoritmo que pida la edad del usuario y 
+//determine si es mayor de edad (18 años o más).
+
+const edad = parseInt(prompt('Ingrese su edad'))
+if( edad >= 18){
+    alert('Sos mayor de edad')
+}else{
+    alert('Sos menor de edad')
+}
