540cf8ea110b56e1f61fa936cbdb0aa0177f0304
Emilse Arias <emilsearias@gmail.com>
2025-04-09 10:33:52 -0300

    agregamos un ejemplo de sintaxis de condicionales simples y dobles

---
+++ js/2-condicionales.js
@@ -0,0 +1,21 @@
+//Estructura de decision
+
+/*
+condicional simple
+if(condicion logica){
+    todas las lineas de codigo que quiero ejecutar si se cumple la condicion
+}
+
+condicional doble
+if(condicion logica){
+    todas las lineas de codigo que quiero ejecutar si se cumple la condicion
+}else{
+    todas las lineas de codigo que quiero ejecutar si no se cumple la condicion
+}
+
+if(condicion logica){
+    todas las lineas de codigo que quiero ejecutar si se cumple la condicion
+}else-if(condicion logica 2){
+    todas las lineas de codigo que quiero ejecutar si no se cumple la condicion
+}
+*/
