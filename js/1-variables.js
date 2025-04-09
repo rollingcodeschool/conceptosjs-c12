8aaf2d5670e78bbd8ebcfd3dd77daefe2356f1f3
Emilse Arias <emilsearias@gmail.com>
2025-04-09 09:36:51 -0300

    cambiamos las variables creadas con let por const

--- js/1-variables.js
+++ js/1-variables.js
@@ -24,14 +24,15 @@ const url = 'http://127.0.0.1:5500/index.html'
 
 document.writeln('<br>El link de live server es: '+ url )
 
-//url = 'nuevo valor'
+// url = 'nuevo valor'
 document.writeln('<br>El link de live server es: '+ url )
 
 //Ejemplo de suma de dos numeros
 // todo: cambiar este ejercicio por constantes
-let numero1, numero2, resultado
-numero1 = 10
-numero2 = 12
-resultado = numero1 + numero2
+// const numero1, numero2, resultado
+const numero1 = 10
+const numero2 = 12
+const resultado = numero1 + numero2
 
-document.writeln('<br>La suma de los dos numeros ingresados es:' + resultado)
+document.writeln('<br>La suma de los dos numeros ingresados es:' + resultado )
+// document.writeln(resultado)
