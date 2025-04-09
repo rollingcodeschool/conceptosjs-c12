92354fb8ef4e510cda08a8932d33f8807967d63b
Emilse Arias <emilsearias@gmail.com>
2025-04-09 10:23:56 -0300

    agregamos variables de ejemplo con cada tipo de dato de javascript

--- js/1-variables.js
+++ js/1-variables.js
@@ -34,3 +34,52 @@ const numero1 = parseInt(prompt('Ingrese un número')),
   numero2 =  parseInt(prompt('Ingrese un segundo número'));
 document.writeln("<br>La suma de los dos numeros ingresados es:" + (numero1 + numero2));
 // document.writeln(resultado)
+
+//tipos de datos primitivos
+//string
+const nombreUsuario = 'Emilse Arias';
+const producto = "Paraguas";
+const saludo = `Hola mundo`;
+//number
+const edad = 30;
+const precio = 3000.50;
+const negativo = -400;
+
+//booleanos
+const esMayordeEdad = true;
+const encendido = false;
+
+//null - este valor indica el vacio
+const informacion = null;
+
+//undefined
+let datosExtras;
+
+//tipos de datos especiales
+//objeto con notacion literal
+const funko = {
+  nombre: 'ironman',
+  numero: 'XSF3434',
+  precio: 30.5
+}
+
+const colores = ['azul','amarillo', 'rojo']
+
+//symbol
+const alumno = Symbol('legajo')
+
+console.log(nombreUsuario);
+console.log(producto);
+console.log(saludo);
+console.log(edad);
+console.log(precio);
+console.log(negativo);
+console.log(esMayordeEdad);
+console.log(encendido);
+console.log(informacion);
+console.log(datosExtras);
+console.log(funko);
+console.log(colores);
+console.log(alumno);
+
+console.log(Number.MAX_SAFE_INTEGER)
