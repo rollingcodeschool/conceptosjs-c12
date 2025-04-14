61fdd8151344523f1025bf0905868425f792a4f3
Emilse Arias <emilsearias@gmail.com>
2025-04-14 11:25:41 -0300

    agregamos la opcion de extraer dinero y cambiamos la opcion por constante

--- js/3-switch.js
+++ js/3-switch.js
@@ -20,7 +20,7 @@ switch(opcion){
 */
 
 //declarar e inicializar variables
-let opcion = parseInt(
+const opcion = (
   prompt(
     "Selecione una opción: 1- consultar el saldo, 2- ingresa dinero, 3- extraer dinero"
   )
@@ -29,6 +29,8 @@ let saldo = 10000;
 
 switch (opcion) {
   case 1:
+  case '1':
+  case 'saldo':
     document.writeln("Su saldo es : $" + saldo);
     break;
   case 2:
@@ -42,5 +44,20 @@ switch (opcion) {
       `<br>Depositaste $${deposito}, tu saldo actual es: $${saldo}`
     );
     break;
-
+  case 3:
+    //preguntar al usuario cuanto quiere extraer
+    const montoExtraer = Number(prompt("Ingrese el monto que desea extraer"));
+    //preguntar si el monto ingresado es valido
+    if (montoExtraer <= saldo) {
+      //extraer el dinero si esta todo bien
+      saldo = saldo - montoExtraer; // saldo -= montoExtraer
+      document.writeln(
+        `Monto a extraer -$${montoExtraer}, tu saldo actual es: $${saldo}`
+      );
+    } else {
+      document.writeln(`Saldo insuficiente`);
+    }
+    break;
+  default:
+    document.writeln("Ingresaste una opción invalida");
 }
