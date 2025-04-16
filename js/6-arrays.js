45dd345c384ce6cc4401a2fdb8d36bf46470f99b
Emilse Arias <emilsearias@gmail.com>
2025-04-16 11:33:28 -0300

    agregamos como declarar una array vacio y uno con datos

---
+++ js/6-arrays.js
@@ -0,0 +1,19 @@
+//declaro un array vacio 
+const alumnos = [];
+
+//declaramos un array con datos
+const frutas = ['manzana',20, true,'🍉','🍊']
+
+//mostrar los datos
+console.log(alumnos)
+console.log(frutas)
+
+document.writeln(alumnos)
+document.writeln('<br>')
+document.writeln(frutas)
+
+document.writeln(`<ul>
+    <li>df</li>
+    <li>sdf</li>
+    <li>sdfsdf</li>
+    </ul>`)
