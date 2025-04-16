fe71953cfa0543266dd46600b8b6980e0652fb52
Emilse Arias <emilsearias@gmail.com>
2025-04-16 10:58:35 -0300

    prueba de scope de una variable

---
+++ js/5-scope.js
@@ -0,0 +1,8 @@
+const opcion = parseInt(prompt('seleccione 1 si quiere ver el saludo, caso contrario presione 2'));
+
+if(opcion === 1){
+    let saludo = 'hola';
+    console.log('1',saludo)
+}else{
+    console.log('segunda prueba',saludo)
+}
