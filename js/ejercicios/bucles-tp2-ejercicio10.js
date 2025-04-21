c7cd152e0c3cda05f82c91ce8227903bddb66204
Emilse Arias <emilsearias@gmail.com>
2025-04-21 09:50:07 -0300

    dibujamos una tabla con las filas y columnas que indica el usuario

---
+++ js/ejercicios/bucles-tp2-ejercicio10.js
@@ -0,0 +1,19 @@
+/*10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.*/
+
+const filas = parseInt(prompt('Ingresa un numero de filas'))
+const columnas = parseInt(prompt('Ingresa un numero de columnas'))
+
+document.writeln(`<table>
+      <tbody>`)
+
+for(let indiceFilas=0; indiceFilas < filas; indiceFilas++){
+    document.writeln(`<tr>`)
+    for(let indiceColumnas=0; indiceColumnas < columnas; indiceColumnas++){
+        document.writeln(`<td>35</td>`)
+    }
+    document.writeln(`</tr>`)
+}
+
+document.writeln(`
+      </tbody>
+    </table>`)
