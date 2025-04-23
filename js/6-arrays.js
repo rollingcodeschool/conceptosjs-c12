e76f94ca4a38a90449a1c0ccfb73911167907d78
Emilse Arias <emilsearias@gmail.com>
2025-04-23 10:54:03 -0300

    acomodamos el codigo del archivo de arrays usando funciones

--- js/6-arrays.js
+++ js/6-arrays.js
@@ -1,100 +1,68 @@
-//declaro un array vacio 
+//declaro un array vacio
 const alumnos = [];
 
 //declaramos un array con datos
-const frutas = ['manzana',20, true,'🍉','🍊']
+const frutas = ["manzana", 20, true, "🍉", "🍊"];
+
+//funciones
+const mostrarFrutas = (titulo) => {
+  document.writeln(`<h3>${titulo}</h3>`);
+  document.writeln(`<ul>`);
+  for (let i = 0; i < frutas.length; i++) {
+    document.writeln(`<li>${frutas[i]}</li>`);
+  }
+  document.writeln(`</ul>`);
+};
 
 //mostrar los datos
-console.log(alumnos)
-console.log(frutas)
+console.log(alumnos);
+console.log(frutas);
 
-document.writeln(alumnos)
-document.writeln('<br>')
-document.writeln(frutas)
-document.writeln('<h2>Mostrar un array</h2>')
+document.writeln(alumnos);
+document.writeln("<br>");
+document.writeln(frutas);
 
 //cuantos elementos tiene el array
-console.log(frutas.length)
+console.log(frutas.length);
 //como accedemos a un elemento de un array
 const posicionSandia = 3;
-console.log(frutas[0])
-console.log(frutas[posicionSandia])
+console.log(frutas[0]);
+console.log(frutas[posicionSandia]);
 
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
+mostrarFrutas('Mostrar array');
 
-document.writeln(`<p>Si accedo a la posicion 4 del array, obtengo: ${frutas[4]} </p>`)
-document.writeln(`<p>Si accedo a la posicion 7 del array, obtengo: ${frutas[7]} </p>`)
+document.writeln(
+  `<p>Si accedo a la posicion 4 del array, obtengo: ${frutas[4]} </p>`
+);
+document.writeln(
+  `<p>Si accedo a la posicion 7 del array, obtengo: ${frutas[7]} </p>`
+);
 
-document.writeln('<h2>Acciones para trabajar con arrays</h2>')
+document.writeln("<h2>Acciones para trabajar con arrays</h2>");
 
-document.writeln('<h3>Agregar elementos en el array (al principio)</h3>')
-//agregar un elemento al principio del array
-frutas.unshift('🍇')
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
+//agregar elementos en el array
+frutas.unshift("🍇");
+mostrarFrutas('Agregar elementos en el array (al principio)')
 
-document.writeln('<h3>Agregar elementos en el array (al final)</h3>')
-frutas.push('🥝', 30)
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
+frutas.push("🥝", 30);
+mostrarFrutas('Agregar elementos en el array (al final)')
 
-document.writeln('<h3>Agregar elementos en el medio del array </h3>')
-frutas.splice(4,0,'🍎')
+frutas.splice(4, 0, "🍎");
+mostrarFrutas('Agregar elementos en el medio del array')
 
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
+//modificar elementos en el array
+frutas[3] = "uvas";
+mostrarFrutas('Modificar elementos del array ')
 
-document.writeln('<h3>Modificar elementos del array </h3>')
+//borrar elementos en el array
+frutas.shift();
+mostrarFrutas('Eliminar el primer elemento del array')
 
-frutas[3] = 'uvas'
+frutas.pop();
+mostrarFrutas('Eliminar el ultimo elemento del array')
 
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
-
-document.writeln('<h3>Eliminar el primer elemento del array </h3>')
-
-frutas.shift()
-
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
-
-document.writeln('<h3>Eliminar el ultimo elemento del array </h3>')
-
-frutas.pop()
-
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
-
-document.writeln('<h3>Eliminar un elemento en el medio del array </h3>')
-
-frutas.splice(1,1)
+frutas.splice(1, 1);
 // frutas.splice(1,3) //borrar mas de un elemento desde la posicion 1
 // frutas.splice(3) //borra todos los elementos desde la posicion 3 en adelante
+mostrarFrutas('Eliminar un elemento en el medio del array')
 
-document.writeln(`<ul>`)
-for(let i=0; i< frutas.length; i++){
-    document.writeln(`<li>${frutas[i]}</li>`)
-}
-document.writeln(`</ul>`)
