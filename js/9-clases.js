39898205cc836f8ebe7486c5b03328970b6af77b
Emilse Arias <emilsearias@gmail.com>
2025-04-30 11:41:40 -0300

    agregamos un ejemplo de clase con herencia

--- js/9-clases.js
+++ js/9-clases.js
@@ -1,17 +1,28 @@
 //nombre, edad, apellido, genero, mail, nombreUsuario, estado
+//asistencia, presentacionPracticos, curso 
 class Persona {
   //lo primero que hacemos es construir las propiedades del objeto
-  
+  //propiedad privada
+  #mail;
   constructor(nombreParam, edadParam, apellidoParam, generoParam, mailParam, nombreUsuarioParam) {
     this.nombre = nombreParam;
     this.edad = edadParam
     this.apellido = apellidoParam
     this.genero = generoParam
-    this.mail = mailParam
+    this.#mail = mailParam
     this.nombreUsuario = nombreUsuarioParam
     this.estado = true //propiedad por defecto
   }
-  //propiedades computadas
+  //propiedades computadas get y set
+get getMail(){
+    return this.#mail
+}
+
+set setMail(nuevoMail){
+    if(nuevoMail.trim().length >0){
+        this.#mail = nuevoMail
+    }
+}
 
   //metodos 
   mostrarDatos(){
@@ -20,11 +31,38 @@ class Persona {
         <li>Apellido: ${this.apellido}</li>
         <li>Edad: ${this.edad}</li>
         <li>Genero: ${this.genero}</li>
-        <li>Email: ${this.mail}</li>
+        <li>Email: ${this.#mail}</li>
         </ul>`)
   }
   
 }
+
+//herencia
+class Alumno extends Persona{
+    // puedo agregar propiedades privadas
+    constructor(nombreParam, edadParam, apellidoParam, generoParam, mailParam, nombreUsuarioParam,  cursoParam){
+       //invocar al constructor de la clase padre
+       super(nombreParam, edadParam,apellidoParam,generoParam,mailParam,nombreUsuarioParam);
+       this.asistencia = 0
+       this.curso = cursoParam
+       this.presentacionPracticos = []
+    }
+    //agregar get, set y metodos
+
+    //polimorfismo
+    mostrarDatos(){
+        document.writeln(`<ul>
+            <li>Nombre: ${this.nombre}</li>
+            <li>Apellido: ${this.apellido}</li>
+            <li>Edad: ${this.edad}</li>
+            <li>Genero: ${this.genero}</li>
+            <li>Email: ${this.getMail}</li>
+            <li>Curso: ${this.curso}</li>
+            </ul>`)
+      }
+}
+
+
 
 //instancia de Persona o debo crear un objeto Persona
 const ximena = new Persona('Ximena',29,'Barrientos','F','ximena@mail.com','ximenaBarrientos')
@@ -34,5 +72,10 @@ console.log(ximena)
 jose.mostrarDatos()
 
 //modificar datos del objeto, por fuera de la clase
-jose.mail = 'joseRomero2@mail.com'
+jose.setMail = 'joseRomero2@mail.com'
 jose.mostrarDatos();
+
+const valentina = new Alumno('Valentina', 30, 'Iramain', 'F', 'valentina@mail.com', 'valentinaIramain', 'Fullstack')
+console.log(valentina)
+valentina.mostrarDatos()
+
